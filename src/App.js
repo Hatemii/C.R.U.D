fe4fd76182bf22d8ce318c59fb5b83c2a06cb936
Hatemii <hatem.xhezairi96@gmail.com>
2020-03-09 16:46:47 +0100

    added .map in UserTable for users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserTable from "./tables/UserTable"
 
 const App = () => {
+
+  const userData = [
+    {id: 1, name: "Mark", username: "M_X_M"},
+    {id: 2, name: "Tom", username: "Tom_123"},
+    {id: 3, name: "Jerry", username: "Jerry_07"},
+  ]
+
+  const [users ,setUsers] = useState(userData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,7 +20,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+          <UserTable users={users}/>
         </div>
       </div>
     </div>
